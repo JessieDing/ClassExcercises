cf95d6e95b2f8bd1243dc7708154f680fa788009
dingjie <nathalie.ding@gmail.com>
2017-02-19 15:21:11 +0800

    modified

--- jessie/day4/Three.java
+++ jessie/day4/Three.java
@@ -3,14 +3,14 @@ package class12.jessie.day4;
 public class Three {
 
 	public static void main(String[] args) {
-		boolean[] arr = new boolean[8];
+		boolean[] arr = new boolean[7];
 		for (int i = 0; i < arr.length; i++) {
-			arr[i] = true;
+			arr[i] = true; // 初始全赋为true
 		}
 
 		int index = 0; // 编号
 		int count = 0; // 报数器
-		int leftNo = arr.length; // 剩余个数，初始为总数
+		int leftNo = arr.length; // 剩余true的个数
 
 		while (leftNo > 2) {
 			if (arr[index] == true) {
@@ -18,12 +18,12 @@ public class Three {
 				if (count == 3) { // 报到3时
 					arr[index] = false;
 					count = 0; // 重新由1开始报数
-					leftNo--; // 将报3的剔除
+					leftNo--;
 				}
 			}
-			index++; // 下一个继续报数
-			if (index == arr.length) { // 最后一个
-				index = 0;
+			index++; // 下一个继续
+			if (index == arr.length) {
+				index = 0; // 不能超出数组长度
 			}
 		}
 		System.out.println("剩余两个：");
