54035bfccec6a44de0b8aaf4c99da6ba5415a45a
dingjie <nathalie.ding@gmail.com>
2017-02-21 18:31:41 +0800

    add files and fix

---
+++ jessie/day5/AmountConvert.java
@@ -0,0 +1,86 @@
+package class12.jessie.day5;
+
+import java.util.Scanner;
+
+public class AmountConvert {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("ÇëÊäÈëÐ¡Ð´½ð¶î£¨ÕûÊý£©£º");
+		int amount = scan.nextInt();
+		System.out.print("×ª»»Îª´óÐ´½ð¶î£º");
+		convert(amount);
+		scan.close();
+	}
+
+	public static void convert(int lowerAmount) {
+		int unit = 10000;
+
+		while (lowerAmount > 0) {
+			int temp = 0;
+			temp = lowerAmount / unit;
+			switch (temp) {
+			case 0:
+				System.out.print("Áã");
+				break;
+			case 1:
+				System.out.print("Ò¼");
+				break;
+			case 2:
+				System.out.print("·¡");
+
+				break;
+			case 3:
+				System.out.print("Èþ");
+				break;
+
+			case 4:
+				System.out.print("ËÁ");
+				break;
+
+			case 5:
+				System.out.print("Îé");
+				break;
+
+			case 6:
+				System.out.print("Â½");
+				break;
+
+			case 7:
+				System.out.print("Æâ");
+				break;
+
+			case 8:
+				System.out.print("°Æ");
+				break;
+
+			case 9:
+				System.out.print("¾Á");
+				break;
+
+			}
+			if (temp != 0) {
+				switch (unit) {
+				case 10000:
+					System.out.print("Íò");
+					break;
+
+				case 1000:
+					System.out.print("Çª");
+					break;
+
+				case 100:
+					System.out.print("°Û");
+					break;
+
+				case 10:
+					System.out.print("Ê°");
+					break;
+
+				}
+			}
+			lowerAmount = lowerAmount % unit;
+			unit = unit / 10;
+		}
+	}
+}
---
+++ jessie/day5/Test.java
@@ -0,0 +1,34 @@
+package class12.jessie.day5;
+
+public class Test {
+
+	public static void main(String[] args) {
+		double amt1 = 5000.00;
+		double amt2 = 300000.00;
+		double amt3 = 2000000.00;
+
+		System.out.println("账户622345000000001手续费" + getFee(amt1));
+		System.out.println("账户622345000000002手续费" + getFee(amt2));
+		System.out.println("账户622345000000003手续费" + getFee(amt3));
+	}
+
+	public static double getRate(double amt) {
+		double rate = 0.00F;
+		if (amt <= 10000) {
+			rate = 0.02;// 2/100两个都是整型，算出来为整型
+			return rate;
+		} else if (amt <= 1000000) {
+			rate = 0.01;
+			return rate;
+		} else {
+			rate = 0.075;// 0.75/100 一个是double，算出来结果为double
+			return rate;
+		}
+	}
+
+	public static double getFee(double amount) {
+		double fee = 0.00F;
+		fee = Math.round(amount * getRate(amount));
+		return fee;
+	}
+}
