f5e29ce2f271075964e8f5d63a93d2424a75653b
dingjie <nathalie.ding@gmail.com>
2017-02-21 16:46:31 +0800

    add method and fix

--- jessie/day5/RemoveBlanc.java
+++ jessie/day5/RemoveBlanc.java
@@ -1,26 +1,38 @@
 package class12.jessie.day5;
 
+import java.util.Arrays;
+
 public class RemoveBlanc {
 	public static char[] trim(char[] c) {
-		char[] newArr = new char[c.length];
+		int lens = c.length - cntSpaceNum(c);
+		char[] newArr = new char[lens];
 		int j = 0;
 		for (int i = 0; i < c.length; i++) {
 			if (c[i] != ' ') {
-				System.arraycopy(c, i, newArr, j, 1);
+				newArr[j] = c[i];
+				// System.arraycopy(c, i, newArr, j, 1);
 				j++;
 			}
 		}
 		return newArr;
 	}
 
+	public static int cntSpaceNum(char[] a) {
+		int num = 0;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == ' ') {
+				num++;
+			}
+		}
+		return num;
+	}
+
 	public static void main(String[] args) {
 		char[] arr = new char[] { '1', ' ', 'w', '3', 'd', ' ', ' ', ' ', ' ', 'y' };
 		System.out.println("原来的数组为：");
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i]);
-		}
-		System.out.println();
+		System.out.println(Arrays.toString(arr));
 		System.out.println("删除空格后的数组为：");
-		System.out.println(trim(arr));
+		System.out.println(Arrays.toString(trim(arr)));
+
 	}
 }
