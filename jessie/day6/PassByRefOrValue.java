d20f5ff9cf33242bb0fa1c824fd502446d308d75
dingjie <nathalie.ding@gmail.com>
2017-02-22 16:40:18 +0800

    add recursion

--- jessie/day6/PassByRefOrValue.java
+++ jessie/day6/PassByRefOrValue.java
@@ -5,10 +5,11 @@ import java.util.Arrays;
 public class PassByRefOrValue {
 
 	public static void main(String[] args) {
+		// 传值
 		int n = 3456;
 		testMethod1(n);
 		System.out.println("main:" + n);
-
+		// 传引用
 		int[] arr = new int[] { 1, 2, 3, 4 };
 		System.out.println("初始数组:" + Arrays.toString(arr));
 		testMethod2(arr);
@@ -16,11 +17,13 @@ public class PassByRefOrValue {
 	}
 
 	public static void testMethod1(int n) {
+		// 传值
 		n = n * 2;
 		System.out.println("testMethod1:" + n);
 	}
 
 	public static void testMethod2(int[] arrSrc) {
+		// 传引用
 		for (int i = 0; i < arrSrc.length; i++) {
 			arrSrc[i] *= 2;
 		}
