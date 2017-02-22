ce8b263c5422b5b802ea7b78f4e7807b7ee8980c
dingjie <nathalie.ding@gmail.com>
2017-02-22 19:17:13 +0800

    add files

--- jessie/day6/ThreeDifNum.java
+++ jessie/day6/ThreeDifNum.java
@@ -4,12 +4,9 @@ public class ThreeDifNum {
 
 	public static void main(String[] args) {
 		int count = 0;
-		int i = 1;
-		int j = 1;
-		int k = 1;
-		for (i = 1; i < 5; i++) {
-			for (j = 1; j < 5; j++) {
-				for (k = 1; k < 5; k++) {
+		for (int i = 1; i < 5; i++) {
+			for (int j = 1; j < 5; j++) {
+				for (int k = 1; k < 5; k++) {
 					if (i != j && i != k && j != k) {
 						count++;
 						System.out.print(i);
