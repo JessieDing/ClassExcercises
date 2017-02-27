3336b725e3221ff21e4d98be63f0ce45a11419b8
dingjie <nathalie.ding@gmail.com>
2017-02-27 13:25:28 +0800

    fixed

--- jessie/d0227/HmwkRev_Bonus.java
+++ jessie/d0227/HmwkRev_Bonus.java
@@ -1,5 +1,7 @@
 package class12.jessie.d0227;
 
+import java.util.Scanner;
+
 //企业发放的奖金根据利润提成。
 //利润(I)低于或等于10万元时，奖金可提10%；
 // 利润高于10万元，低于20万元时，低于10万元的部分按10%提成，高于10万元的部分，可提成7.5%；
@@ -10,39 +12,43 @@ package class12.jessie.d0227;
 // 从键盘输入当月利润I，求应发放奖金总数？
 public class HmwkRev_Bonus {
 	public static void main() {
-
+		Scanner scan = new Scanner(System.in);
+		System.out.println("请输入当月利润：");
+		double i = scan.nextDouble();
+		System.out.println("应发奖金总数为：" + calcBonus(i));
+		System.out.println();
+		scan.close();
 	}
 
 	public static double calcBonus(double margin) {
-		double money = 0;
 		double temp = 0;
 		double bonus = 0;
 		if (margin > 1000000) {
 			temp = margin - 1000000.00;
-			bonus = temp * 0.10;
-			money -= temp;
+			bonus += temp * 0.10;
+			margin -= temp;
 		}
 		if (margin > 600000) {
 			temp = margin - 600000.00;
-			bonus = temp * 0.015;
-			money -= temp;
+			bonus += temp * 0.015;
+			margin -= temp;
 		}
 		if (margin > 400000) {
 			temp = margin - 400000.00;
-			bonus = temp * 0.03;
-			money -= temp;
+			bonus += temp * 0.03;
+			margin -= temp;
 		}
 		if (margin > 200000) {
 			temp = margin - 200000.00;
-			bonus = temp * 0.05;
-			money -= temp;
+			bonus += temp * 0.05;
+			margin -= temp;
 		}
 		if (margin > 100000) {
 			temp = margin - 100000.00;
-			bonus = temp * 0.075;
-			money -= temp;
+			bonus += temp * 0.075;
+			margin -= temp;
 		}
-		bonus = margin * 0.10;
+		bonus += margin * 0.10;
 		return bonus;
 	}
 
