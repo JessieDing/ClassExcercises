ab446a7b123bd4845aa64cd3c125c18937113bd2
dingjie <nathalie.ding@gmail.com>
2017-03-02 18:44:25 +0800

    homework

---
+++ jessie/d0302/AcctTest.java
@@ -0,0 +1,111 @@
+package class12.jessie.d0302;
+
+public class AcctTest {
+
+	public static void main(String[] args) {
+		ZSAcct zhangsan = new ZSAcct("6222020000000000001", "张三", 1, 0.00);
+		System.out.println("张三存了2000元：");
+		zhangsan.deposit(2000.00);
+		zhangsan.printInfo();
+		System.out.println("张三取了1000元：");
+		zhangsan.draw(1000.00);
+		zhangsan.printInfo();
+		double intereAmt = zhangsan.calcInterest(zhangsan.getBalance());
+		System.out.println("张三结息：" + intereAmt);
+		System.out.println("张三销户：");
+		zhangsan.setStatus(2);
+		zhangsan.acctStatus();
+	}
+
+}
+
+class ZSAcct extends Acct {
+	ZSAcct(String acct_no, String acct_name, int status, double balance) {
+		setAcct_no(acct_no);
+		setAcct_name(acct_name);
+		setStatus(status);
+		setBalance(balance);
+	}
+}
+
+class Acct {
+
+	private String acct_no;
+	private String acct_name;
+	private int status;
+	private double balance;
+
+	public String getAcct_no() {
+		return acct_no;
+	}
+
+	public void setAcct_no(String acct_no) {
+		this.acct_no = acct_no;
+	}
+
+	public String getAcct_name() {
+		return acct_name;
+	}
+
+	public void setAcct_name(String acct_name) {
+		this.acct_name = acct_name;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	// 存款
+	public double deposit(double amt) {
+		balance = balance + amt;
+		return balance;
+	}
+
+	// 取款
+	public double draw(double amt) {
+		balance = balance - amt;
+		return balance;
+	}
+
+	// 结息
+	public double calcInterest(double amt) {
+		double interest = balance * 0.02;
+		return interest;
+	}
+
+	// 显示账户状态
+	public void acctStatus() {
+		switch (getStatus()) {
+		case 1:
+			System.out.println(acct_name + "," + acct_no + "," + "账户状态：正常");
+			break;
+		case 2:
+			System.out.println(acct_name + "," + acct_no + "," + "账户状态：销户");
+			break;
+		case 3:
+			System.out.println(acct_name + "," + acct_no + "," + "账户状态：冻结");
+			break;
+		case 4:
+			System.out.println(acct_name + "," + acct_no + "," + "账户状态：挂失");
+			break;
+		}
+	}
+
+	// 打印账户信息
+	public void printInfo() {
+		System.out.println(acct_name + "," + acct_no + "," + "账户余额：" + balance);
+	}
+
+}
